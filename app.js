a4472ca20bdbcf7c4c441c10fcb026f323e848ed
Maxirc85@hotmail.com <maxirc85@hotmail.com>
2023-03-28 01:45:45 -0300

    varios

---
+++ app.js
@@ -0,0 +1,70 @@
+import { Producto } from "./Product.js";
+import { UI } from "./UI.js";
+
+// DOM Events
+document.getElementById("product-form")
+
+document.addEventListener("submit", function (e) {
+    // Override the default Form behaviour
+    e.preventDefault();
+
+    // Getting Form Values
+    const nombre = document.getElementById("nombre").value;
+    const precio = document.getElementById("precio").value;
+    const cantidad = document.getElementById("cantidad").value;
+
+    // Create a new Oject Product
+    const producto = new Producto(nombre, precio, cantidad);
+
+    // Create a new UI instance
+    const ui = new UI();
+
+    // Input User Validation
+    if (nombre === "" || precio === "" || cantidad === "") {
+      ui.mostrarMensaje("Por favor ingrese datos en todos los campos.", "danger");
+    }
+
+    // Save Product
+    ui.agregarProductos(producto);
+    ui.mostrarMensaje("Producto agregado correctamente", "success");
+    ui.resetearForm();
+  });
+
+document.getElementById("product-list").addEventListener("click", (e) => {
+  const ui = new UI();
+  ui.eliminaProductos(e.target);
+  e.preventDefault();
+});
+
+obtener_bienvenida();
+guardar_bienvenida();
+
+function obtener_bienvenida(){
+  if (localStorage.getItem("nombre")){
+    let nombre = localStorage.getItem ("nombre");
+    let persona = JSON.parse(localStorage.getItem("persona"));
+    
+    console.log(nombre);
+    console.log(persona);
+ 
+  } else {
+    console.log("No hay nada.")
+  }
+    
+}
+
+function guardar_bienvenida(){
+  
+  let persona = {
+    nombre:   "Maxi",
+    edad: 31,
+    apellido:  "pepe",
+  }
+};
+
+let nombre = prompt("Ingrese su nombre.");
+
+localStorage.setItem("nombre", nombre);
+localStorage.setItem("persona", JSON.stringify (persona));
+
+document.body.innerHTML = nombre;
